69e600ab5369757685ca34034e5da5bbad3f98f1
Srujan SS <srujanss2977@gmail.com>
2026-08-20 23:50:41 +0530

    Time: 7 ms (12.76%), Space: 12.5 MB (72.04%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isValid(self, s):
+        stack = []
+        matching = {')' : '(' ,'}':'{',']':'['}
+        for char in s:
+            if char in matching.values():
+                stack.append(char)
+            elif char in matching:
+                if not stack or stack.pop()!= matching[char]:
+                    return False
+        return len(stack) == 0
